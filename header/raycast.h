3ac3a3161d77e569291fb162a98f6a97f140a056
leesiha <mlice1030@soongsil.ac.kr>
2024-01-24 20:18:24 +0900

    raycasting code finish. have to make game window

--- header/raycast.h
+++ header/raycast.h
@@ -6,7 +6,7 @@
 /*   By: sihlee <sihlee@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 16:56:44 by sihlee            #+#    #+#             */
-/*   Updated: 2024/01/21 17:27:43 by sihlee           ###   ########.fr       */
+/*   Updated: 2024/01/24 20:07:03 by sihlee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,37 @@
 # define RAYCAST_H
 
 # include "cub3d.h"
+# include <stdio.h>
+# include <math.h>
+# include <mlx.h>
 
-typedef struct s_vector
+// typedef struct s_player
+// {
+// 	// 캐릭터 위치(좌표)
+// 	double	pos_x;
+// 	double	pos_y;
+// 	// 캐릭터 벡터
+// 	double	dir_xv;
+// 	double	dir_yv;
+// 	// 카메라평면 벡터(카메라 평면 == 플레이어가 게임창으로 보는 전경 == 게임창)
+// 	double	plane_xv;
+// 	double	plane_yv;
+// }	t_player;
+
+// mlx 구조체 mlx 포인터와 생성할 win 포인터를 가지고 있다.
+typedef struct	s_vars {
+	void		*mlx;
+	void		*win;
+}	t_vars;
+
+//이미지의 정보를 나타내는 변수를 저장한 구조체
+typedef struct s_data
 {
-	double	pos_x; //플레이어의 초기 위치벡터
-	double	pos_y;
-	double	dir_x; //플레이어의 초기 방향벡터
-	double	dir_y;
-	double	plane_x; //플레이어의 카메라평면
-	double	plane_y;
-	double	ray_dir_x; //광선의 방향벡터
-	double	ray_dir_y;
-}	t_vector;
-
-typedef struct s_player
-{
-	/* data */
-}	t_player;
-
+	void 	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		size_line;
+	int		endian;
+}	t_data;
 
 #endif
